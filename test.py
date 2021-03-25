bc2b17c86c881d51fff658f8fbbaf19b2087b6f9
lingulist <mattis.list@lingpy.org>
2021-03-25 15:48:19 +0100

    update new files

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
-
 def test_valid(cldf_dataset, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
+
 
 def test_forms(cldf_dataset):
     assert len(list(cldf_dataset["FormTable"])) == 910
 
+
 def test_parameters(cldf_dataset):
     assert len(list(cldf_dataset["ParameterTable"])) == 315
 
+
 def test_languages(cldf_dataset):
     assert len(list(cldf_dataset["LanguageTable"])) == 12
+
 
 def test_cognates(cldf_dataset):
     assert len(list(cldf_dataset["CognateTable"])) == 910
